399bd6ab15c9cbd2da89946a9579bb2ff60a2e22
zishanshan0115 <zishanshan0115@gmail.com>
2025-03-18 23:55:20 +0800

    V.1

---
+++ 吧噗冰淇淋.js
@@ -0,0 +1,11 @@
+const audioPlayer = document.getElementById('audioPlayer');
+function playMedia() {
+    var audioPlayer =  document.getElementById('audioPlayer');  
+    audioPlayer.play();
+}
+
+
+function pauseMedia() { 
+    var audioPlayer =  document.getElementById('audioPlayer');
+    audioPlayer.pause();
+}
