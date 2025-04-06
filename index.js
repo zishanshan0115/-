bf9b721b7118fc765d6395b0982fa736526f8c17
zishanshan0115 <zishanshan0115@gmail.com>
2025-04-07 02:23:46 +0800

    V.4
    Signed-off-by: zishanshan0115 <zishanshan0115@gmail.com>

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ document.addEventListener('DOMContentLoaded',function () {
     });
 }); 
 
+const audioPlayer = document.getElementById('audioPlayer');
+function playMedia() {
+    var audioPlayer =  document.getElementById('audioPlayer');  
+    audioPlayer.play();
+}
+
+
+function pauseMedia() { 
+    var audioPlayer =  document.getElementById('audioPlayer');
+    audioPlayer.pause();
+}
